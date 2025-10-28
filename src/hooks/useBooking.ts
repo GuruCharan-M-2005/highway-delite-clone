1b9e421e16a7960c6f46fc03628ffbf4a302545e
GuruCharan-M-2005 <gurumrdg005@gmail.com>
2025-10-29 00:04:38 +0530

    ready to deploy

--- src/hooks/useBooking.ts
+++ src/hooks/useBooking.ts
@@ -110,37 +110,3 @@ export const useBooking = create<BookingStore>((set, get) => ({
 }));
 
 
-
-// import { create } from 'zustand';
-// import { Booking, BookingDetails, UserInfo } from '@/types/booking';
-
-// interface BookingStore {
-//   bookingDetails: Partial<BookingDetails>;
-//   userInfo: Partial<UserInfo>;
-//   promoCode: string;
-//   setBookingDetails: (details: Partial<BookingDetails>) => void;
-//   setUserInfo: (info: Partial<UserInfo>) => void;
-//   setPromoCode: (code: string) => void;
-//   resetBooking: () => void;
-// }
-
-// export const useBooking = create<BookingStore>((set) => ({
-//   bookingDetails: {},
-//   userInfo: {},
-//   promoCode: '',
-//   setBookingDetails: (details) =>
-//     set((state) => ({
-//       bookingDetails: { ...state.bookingDetails, ...details },
-//     })),
-//   setUserInfo: (info) =>
-//     set((state) => ({
-//       userInfo: { ...state.userInfo, ...info },
-//     })),
-//   setPromoCode: (code) => set({ promoCode: code }),
-//   resetBooking: () =>
-//     set({
-//       bookingDetails: {},
-//       userInfo: {},
-//       promoCode: '',
-//     }),
-// }));
